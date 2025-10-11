53a777eef883460bdfbf538454d7739454b6bd87
Schiggy <hillbrecht.s@gmail.com>
2025-10-11 23:01:06 +0200

    working on charakter creation

--- hero_creation/hero_creation.gd
+++ hero_creation/hero_creation.gd
@@ -1,9 +1,24 @@
 extends CanvasLayer
 
+@onready var class_btn: Button = %ClassBtn
+@onready var list_of_classes: VBoxContainer = %ListOfClasses
+
 var index = 0
+
+func _ready() -> void:
+	list_of_classes.visible = false
 
 func _on_x_btn_pressed() -> void:
 	queue_free()
 
 func _on_accept_btn_pressed() -> void:
 	queue_free()
+
+func _on_class_btn_pressed() -> void:
+	list_of_classes.visible = !list_of_classes.visible
+
+func _on_race_btn_pressed() -> void:
+	list_of_classes.visible = false
+
+func _on_zodiac_btn_pressed() -> void:
+	list_of_classes.visible = false
