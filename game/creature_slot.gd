a38e3250e8f786b1baab2bb652da519d57f58ffb
Schiggy <hillbrecht.s@gmail.com>
2025-10-11 20:28:51 +0200

    removed unused font

--- game/creature_slot.gd
+++ game/creature_slot.gd
@@ -1,4 +1,14 @@
 extends Marker2D
 
+@onready var button: Button = %Button
+@onready var pointer: Sprite2D = %Pointer
+
 func _ready() -> void:
-	pass
+	pointer.visible = false
+
+func _on_button_mouse_entered() -> void:
+	if button.disabled: return
+	pointer.visible = true
+
+func _on_button_mouse_exited() -> void:
+	pointer.visible = false
