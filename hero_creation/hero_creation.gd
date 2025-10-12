4748a721c1bd73c349c981df1611d9eafbaa9861
Schiggy <hillbrecht.s@gmail.com>
2025-10-12 09:05:34 +0200

    testing arround with attributes

--- hero_creation/hero_creation.gd
+++ hero_creation/hero_creation.gd
@@ -2,11 +2,16 @@ extends CanvasLayer
 
 @onready var role_btn: Button = %RoleBtn
 @onready var roles_container: VBoxContainer = %RolesContainer
+@onready var label_strength: Label = %LabelStrength
+@onready var label_dex: Label = %LabelDex
+@onready var label_charm: Label = %LabelCharm
+@onready var label_wits: Label = %LabelWits
 
 var bp_role_btn = preload("res://hero_creation/role_btn.tscn")
 var index = 0
 
 func _ready() -> void:
+	SignalManager.update_actor.connect(_on_update_actor)
 	roles_container.visible = false
 	ActorManager.create_hero(
 		ActorManager.hero_roles.pick_random(),
@@ -18,6 +23,16 @@ func _ready() -> void:
 		btn.index = index
 		btn.role = role
 		roles_container.add_child(btn)
+	SignalManager.update_actor.emit()
+
+func _on_update_actor(_index):
+	_update_attributes()
+
+func _update_attributes():
+	label_strength.text = "Strength: %s" % GameData.actors[index].strength
+	label_dex.text = "Dex: %s" % GameData.actors[index].dex
+	label_charm.text = "Charm: %s" % GameData.actors[index].charm
+	label_wits.text = "Wits: %s" % GameData.actors[index].wits
 
 func _on_x_btn_pressed() -> void:
 	ActorManager.remove_hero(index)
