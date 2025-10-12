a11caabbb65c182723d21456fdf8fc84d45764c0
Schiggy <hillbrecht.s@gmail.com>
2025-10-12 15:39:18 +0200

    refined role and background selector

--- hero_creation/role_and_background_selector.gd
+++ hero_creation/role_and_background_selector.gd
@@ -9,18 +9,34 @@ func _ready() -> void:
 	_start_background_menu()
 
 func _start_role_menu():
-	roles_menu.get_popup().canvas_item_default_texture_filter = Viewport.DEFAULT_CANVAS_ITEM_TEXTURE_FILTER_NEAREST
+	# set filter
+	var popup = roles_menu.get_popup()
+	popup.canvas_item_default_texture_filter = Viewport.DEFAULT_CANVAS_ITEM_TEXTURE_FILTER_NEAREST
+	# clear
 	roles_menu.clear()
+	# fill
+	await get_tree().process_frame
+	var selected_id = GameData.actors[index].id
 	for i in ActorManager.hero_roles.size():
 		var role = ActorManager.hero_roles[i]
 		roles_menu.add_item(Utils.id_to_string(role.id))
+		if role.id == selected_id:
+			roles_menu.select(i)
 
 func _start_background_menu():
-	backgrounds_menu.get_popup().canvas_item_default_texture_filter = Viewport.DEFAULT_CANVAS_ITEM_TEXTURE_FILTER_NEAREST
+	# set filter
+	var popup = backgrounds_menu.get_popup()
+	popup.canvas_item_default_texture_filter = Viewport.DEFAULT_CANVAS_ITEM_TEXTURE_FILTER_NEAREST
+	# clear
 	backgrounds_menu.clear()
+	# fill
+	await get_tree().process_frame
+	var selected_id = GameData.actors[index].background
 	for i in ActorManager.hero_backgrounds.size():
 		var background = ActorManager.hero_backgrounds[i]
 		backgrounds_menu.add_item(Utils.id_to_string(background.id))
+		if background.id == selected_id:
+			backgrounds_menu.select(i)
 
 func _on_roles_menu_item_selected(_index: int) -> void:
 	_create_hero()
--- singeltons/utils.gd
+++ singeltons/utils.gd
@@ -8,3 +8,10 @@ func id_to_string(txt: String) -> String:
 		if words[i].length() > 0:
 			words[i] = words[i].substr(0, 1).to_upper() + words[i].substr(1).to_lower()
 	return " ".join(words)
+
+func string_to_id(txt: String) -> String:
+	var words := txt.strip_edges().split(" ", false)
+	for i in range(words.size()):
+		if words[i].length() > 0:
+			words[i] = words[i].to_lower()
+	return "_".join(words)
