7f2ab9b638409c841d479723334f53eb7859078a
Schiggy <hillbrecht.s@gmail.com>
2025-10-16 20:44:15 +0200

    added quest done screen

---
+++ quest_manager/quest_done.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func _on_background_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_continue_btn_pressed() -> void:
+	pass # Replace with function body.
