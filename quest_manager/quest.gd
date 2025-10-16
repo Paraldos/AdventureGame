35b7c1a063b3922c9f6e3be719e983490ec6de1c
Schiggy <hillbrecht.s@gmail.com>
2025-10-16 15:15:19 +0200

    refactored artwork

---
+++ quest_manager/quest.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name Quest
+
+@export var title = ""
---
+++ quest_manager/quest_manager.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func add_quest():
+	pass
+
+func remove_quest():
+	pass
+
+func solve_quest():
+	pass
