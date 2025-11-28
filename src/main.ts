cac47533eb8c5832d3334fb6a14d242f9bdd716d
Schiggy <hillbrecht.s@gmail.com>
2025-11-28 20:38:48 +0100

    basic kaplay setup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import "./style.css";
+import kaplay from "kaplay";
 
-console.log("bert");
+const k = kaplay({
+  width: 200,
+  height: 200,
+  background: "#d46eb3",
+});
+const player = k.add([k.rect(40, 60), k.pos(10, 10)]);
