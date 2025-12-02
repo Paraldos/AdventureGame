836074a99b72c7c1c757461c040d5dba3958ea39
Schiggy <hillbrecht.s@gmail.com>
2025-12-02 19:30:41 +0100

    adjusted player

--- src/scenes/map/Player.ts
+++ src/scenes/map/Player.ts
@@ -1,7 +1,22 @@
-import type { KAPLAYCtx } from "kaplay";
+import type { KAPLAYCtx, GameObj } from "kaplay";
 
 export default class Player {
+  k: KAPLAYCtx;
+  mainSprite?: GameObj;
+  posX: number = 15;
+  posY: number = 15;
+
   constructor(k: KAPLAYCtx) {
+    this.k = k;
     console.log("player");
+    this.drawMainSprite();
+  }
+
+  drawMainSprite(): void {
+    this.mainSprite = this.k.add([
+      this.k.sprite("player"),
+      this.k.pos(this.posX, this.posY),
+      this.k.anchor("center"),
+    ]);
   }
 }
